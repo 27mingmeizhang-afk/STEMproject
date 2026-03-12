34a8232c0ab6202f31c42e503b282b7425edc1a7
27mingmeizhang-afk <27mingmei.zhang@cats-fc.org>
2026-03-12 14:28:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 let score = 0;
 
 function increaseScore(){
-score++;
-document.getElementById("score").innerText = score;
+    score++;
+    document.getElementById("score").innerText = score;
 }
 
 function uploadFile(){
-
-let file = document.getElementById("fileUpload").files[0];
-
-if(file){
-document.getElementById("uploadStatus").innerText =
-"File uploaded: " + file.name;
-}else{
-document.getElementById("uploadStatus").innerText =
-"Please choose a file first.";
-}
-
+    let file = document.getElementById("fileUpload").files[0];
+    if(file){
+        document.getElementById("uploadStatus").innerText = "File uploaded: " + file.name;
+    }else{
+        document.getElementById("uploadStatus").innerText = "Please choose a file first.";
+    }
 }
